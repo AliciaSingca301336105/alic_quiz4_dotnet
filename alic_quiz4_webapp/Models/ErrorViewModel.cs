477403e33be12896e88debecdece37f7c16747c2
Alicia Singca <asingca@my.centennialcollege.ca>
2025-04-07 11:55:57 -0400

    Add project files.

---
+++ alic_quiz4_webapp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace alic_quiz4_webapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
